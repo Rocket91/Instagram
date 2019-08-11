d2eba1602d8f75c253f077c5b7bf81f9a011ef32
Rocket91 <jeancharlesyvenson@hotmail.com>
2019-08-11 13:36:35 -0400

    initial commit

---
+++ app/src/main/java/com/applicatech/instagram/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.applicatech.instagram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private EditText etUserName;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        etUserName = findViewById(R.id.etUserName);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUserName.getText().toString();
+                String password = etPassword.getText().toString();
+                login(username, password);
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    // TODO: better error notifications to users
+                    Log.e(TAG, "issue with login");
+                    e.printStackTrace();
+                    return;
+                }
+                //TODO: navigate the new activity if the user has signed properly
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Log.d(TAG, "Navigating to MainActivity");
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/applicatech/instagram/MainActivity.java
@@ -0,0 +1,52 @@
+package com.applicatech.instagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.etDescription);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        //QueryPost();
+
+    }
+
+    private void QueryPost() {
+        ParseQuery<Post> postQuery = new ParseQuery<Post>(Post.class);
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                for (int i = 0; i < posts.size(); i++){
+                    Log.d(TAG, "post: "+posts.get(i).getDescription());
+                }
+            }
+        });
+    }
+}
