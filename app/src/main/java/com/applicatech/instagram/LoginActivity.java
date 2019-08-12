8e0b424c111d10ae6e95ca4330589860fce436cb
Rocket91 <jeancharlesyvenson@hotmail.com>
2019-08-12 15:33:49 -0400

    sign up done

--- app/src/main/java/com/applicatech/instagram/LoginActivity.java
+++ app/src/main/java/com/applicatech/instagram/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.applicatech.instagram;
 
+import android.app.Notification;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
@@ -19,11 +24,38 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUserName;
     private EditText etPassword;
     private Button btnLogin;
+    private TextView tvSignUp;
+    private AnimationDrawable animationDrawable;
+    RelativeLayout relativeLayout;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+
+            animationDrawable.stop();
+        }
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+
+            animationDrawable.start();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        relativeLayout = findViewById(R.id.relativeLayout);
+
+        animationDrawable =(AnimationDrawable)relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
 
         etUserName = findViewById(R.id.etUserName);
         etPassword = findViewById(R.id.etPassword);
@@ -36,6 +68,18 @@ public class LoginActivity extends AppCompatActivity {
                 login(username, password);
             }
         });
+
+        tvSignUp = findViewById(R.id.tvSignUp);
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
 
     private void login(String username, String password) {
---
+++ app/src/main/java/com/applicatech/instagram/SignUpActivity.java
@@ -0,0 +1,113 @@
+package com.applicatech.instagram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText etUsername2;
+    private EditText etEmailSignup;
+    private EditText etPassword2;
+    private Button btnSignUp;
+    private TextView tvSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        etUsername2 = findViewById(R.id.etUserName2);
+        etEmailSignup = findViewById(R.id.etEmailSignup);
+        etPassword2 = findViewById(R.id.etPassword2);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        tvSignUp = findViewById(R.id.tvSignUp);
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View view) {
+
+                if(etUsername2.getText().toString().isEmpty() || etPassword2.getText().toString().isEmpty()){
+
+                    Toast.makeText(SignUpActivity.this, "Username and password are required", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+
+                String username = etUsername2.getText().toString();
+
+                String email = etEmailSignup.getText().toString();
+
+                String password = etPassword2.getText().toString();
+
+
+
+                ParseUser user = new ParseUser();
+
+                user.setUsername(username);
+
+                user.setEmail(email);
+
+                user.setPassword(password);
+
+
+
+                user.signUpInBackground(new SignUpCallback() {
+
+                    @Override
+
+                    public void done(ParseException e) {
+
+                        if(e == null){
+
+                            etUsername2.setText("");
+
+                            etEmailSignup.setText("");
+
+                            etPassword2.setText("");
+
+                            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+
+                            startActivity(i);
+
+                        }else{
+
+                            ParseErrorHandler.handleParseError(e);
+
+                        }
+
+                    }
+
+                });
+
+            }
+
+        });
+
+    }
+
+
+
+}
